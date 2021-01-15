deff439e9c1d735c827bc1aaa7fc1f1d69228959
DESKTOP-2D8A55Q\Ishara Lakshan <ishara@tuta.io>
2021-01-15 16:04:42 +0530

    include header-css.php & footer-js.php to index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <?php include 'includes/header-css.php';?>
+
+
+    <title>Hello, world!</title>
+</head>
+<body>
+<h1>Hello, world!</h1>
+
+   <?php include 'includes/footer-js.php';?>
+
+</body>
+</html>
